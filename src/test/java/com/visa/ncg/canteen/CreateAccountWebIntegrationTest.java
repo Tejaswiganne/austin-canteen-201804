96f0fd1defbb51557ef6cd5e43e43286a3f07d7c
Ted M. Young <tedyoung@gmail.com>
2018-04-20 10:26:45 -0500

    Lab 12: post with redirect to all accounts.

--- src/test/java/com/visa/ncg/canteen/CreateAccountWebIntegrationTest.java
+++ src/test/java/com/visa/ncg/canteen/CreateAccountWebIntegrationTest.java
@@ -5,10 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -29,4 +32,17 @@ public class CreateAccountWebIntegrationTest {
            .andExpect(view().name("create-account"));
   }
 
+  @Test
+  public void postToCreateAccountCreatesAccountAndRedirects() throws Exception {
+    MvcResult mvcResult = mockMvc.perform(
+        post("/create-account")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("accountName", "Video Games")
+    )
+                                 .andExpect(status().is3xxRedirection())
+                                 .andReturn();
+    mockMvc.perform(get(mvcResult.getResponse().getRedirectedUrl()))
+           .andExpect(status().isOk())
+           .andExpect(view().name("all-accounts"));
+  }
 }
